43ed2a77bae2a374533b9deac207728cdf251927
Montse Lozano Dieck <montse_967@hotmail.com>
2014-02-05 21:08:04 -0600

    proyectos

---
+++ Prueba.h
@@ -0,0 +1,13 @@
+//
+//  Prueba.h
+//  
+//
+//  Created by Maria Montserrat Lozano on 15/01/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Prueba : NSObject
+
+@end
---
+++ Prueba.m
@@ -0,0 +1,13 @@
+//
+//  Prueba.m
+//  
+//
+//  Created by Maria Montserrat Lozano on 15/01/14.
+//
+//
+
+#include <stdio.h>
+int main(){
+    printf("Hello World\n");
+    return 0;
+}

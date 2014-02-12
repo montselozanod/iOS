7da4662054d3235f03765304323ba71336d96241
Montse Lozano Dieck <montse_967@hotmail.com>
2014-02-12 10:45:33 -0600

    new project storyboard

---
+++ Storyboard/Storyboard/StoryAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StoryAppDelegate.h
+//  Storyboard
+//
+//  Created by Maria Montserrat Lozano on 12/02/14.
+//  Copyright (c) 2014 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Storyboard/Storyboard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Storyboard
+//
+//  Created by Maria Montserrat Lozano on 12/02/14.
+//  Copyright (c) 2014 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StoryAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StoryAppDelegate class]));
+    }
+}
